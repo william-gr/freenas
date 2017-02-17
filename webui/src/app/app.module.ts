29f6e0ce577f06ebfbd2c89ee64a2e92322723d5
William Grzybowski <wg@FreeBSD.org>
2017-02-17 18:54:12 -0200

    fix webpack busy and list
    Ticket:	#

--- webui/src/app/app.module.ts
+++ webui/src/app/app.module.ts
@@ -18,6 +18,8 @@ import { GlobalState } from './global.state';
 import { NgaModule } from './theme/nga.module';
 import { PagesModule } from './pages/pages.module';
 
+import 'style-loader!angular2-busy/build/style/busy.css';
+
 // Application wide providers
 const APP_PROVIDERS = [
   AppState,
