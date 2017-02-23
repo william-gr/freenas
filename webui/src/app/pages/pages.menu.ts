b5b2339fadac78076aa7189afde3e1edde8e5ac1
William Grzybowski <wg@FreeBSD.org>
2017-02-23 09:22:24 -0300

    submenu
    Ticket:	#

--- webui/src/app/pages/pages.menu.ts
+++ webui/src/app/pages/pages.menu.ts
@@ -8,20 +8,26 @@ export const PAGES_MENU = [
     order: 0
   },
   {
-    path: ['users'],
-    title: 'Users',
-    icon: 'ion-person',
-    selected: false,
-    expanded: false,
-    order: 0
-  },
-  {
-    path: ['groups'],
-    title: 'Groups',
+    title: 'Accounts',
     icon: 'ion-person-stalker',
-    selected: false,
-    expanded: false,
-    order: 0
+    children: [
+      {
+        path: ['users'],
+        title: 'Users',
+        icon: 'ion-person',
+        selected: false,
+        expanded: false,
+        order: 0
+      },
+      {
+        path: ['groups'],
+        title: 'Groups',
+        icon: 'ion-person-stalker',
+        selected: false,
+        expanded: false,
+        order: 0
+      },
+    ],
   },
   {
     path: ['interfaces'],
@@ -40,28 +46,34 @@ export const PAGES_MENU = [
     order: 0
   },
   {
-    path: ['sharing', 'afp'],
-    title: 'AFP',
-    icon: 'ion-social-apple',
-    selected: false,
-    expanded: false,
-    order: 0
-  },
-  {
-    path: ['sharing', 'nfs'],
-    title: 'NFS',
-    icon: 'ion-social-freebsd-devil',
-    selected: false,
-    expanded: false,
-    order: 0
-  },
-  {
-    path: ['sharing', 'smb'],
-    title: 'SMB',
-    icon: 'ion-social-windows',
-    selected: false,
-    expanded: false,
-    order: 0
+    title: 'Sharing',
+    icon: 'ion-share',
+    children: [
+      {
+        path: ['sharing', 'afp'],
+        title: 'AFP',
+        icon: 'ion-social-apple',
+        selected: false,
+        expanded: false,
+        order: 0
+      },
+      {
+        path: ['sharing', 'nfs'],
+        title: 'NFS',
+        icon: 'ion-social-freebsd-devil',
+        selected: false,
+        expanded: false,
+        order: 0
+      },
+      {
+        path: ['sharing', 'smb'],
+        title: 'SMB',
+        icon: 'ion-social-windows',
+        selected: false,
+        expanded: false,
+        order: 0
+      },
+    ],
   },
   {
     path: ['services'],
