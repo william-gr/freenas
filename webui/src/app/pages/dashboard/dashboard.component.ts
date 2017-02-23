6be55c896fb1dc746d52da47e4b57e910d95f42d
William Grzybowski <wg@FreeBSD.org>
2017-02-23 13:30:42 -0300

    dashboard WIP
    Ticket:	#

--- webui/src/app/pages/dashboard/dashboard.component.ts
+++ webui/src/app/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,25 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { WebSocketService } from '../../services/';
 
 @Component({
   selector: 'dashboard',
   styleUrls: ['./dashboard.scss'],
   templateUrl: './dashboard.html'
 })
-export class Dashboard {
+export class Dashboard implements OnInit {
 
-  constructor() {
+  protected info: any = {};
+
+  constructor(private ws: WebSocketService) {
+  }
+
+  ngOnInit() {
+    this.ws.call('system.info').subscribe((res) => {
+      this.info = res;
+      this.info.loadavg = this.info.loadavg.map((x, i) => { return x.toFixed(2); }).join(' ');
+      this.info.physmem = Number(this.info.physmem / 1024 / 1024).toFixed(0) + ' MiB';
+    });
   }
 
 }
